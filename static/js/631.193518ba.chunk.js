3b5b8f89f6bc84521f27eda0ed4cfb9a9764438d
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-01-12 22:29:14 +0000

    Deploying to gh-pages from @ KaterynaMal/goit-react-hw-05-movies@7f1d2ddb704cdb2baee84b48a004185df3941cd2 🚀

--- static/js/631.193518ba.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[631],{631:function(e,t,a){a.r(t);a(791);var r=a(184);t.default=function(){return(0,r.jsx)("div",{children:"CastPage"})}}}]);
-//# sourceMappingURL=631.193518ba.chunk.js.map
