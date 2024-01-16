142b4574d01caf0b89a2d5945af873b6cc4772d5
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-01-16 17:36:15 +0200

    css

--- src/components/GoBackBtn/GoBackBtn.jsx
+++ src/components/GoBackBtn/GoBackBtn.jsx
@@ -1,12 +1,15 @@
 import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
+import css from './GoBackBtn.module.css'
 
 const GoBackBtn = ({ location }) => {
   const backLinkRef = useRef(location.state?.from ?? '/');
 
   return (
     <div>
-      <Link to={backLinkRef.current}>Go back</Link>
+      <Link to={backLinkRef.current}>
+        <button type='button' className={css.backBtn}>Go Back</button>
+      </Link>
     </div>
   );
 };
